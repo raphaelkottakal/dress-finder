094c557e44a5738c02b42421faea96e6b682981e
Raphael Kottakal <raphaelsemail@gmail.com>
2016-10-12 17:19:47 +0530

    render questions from data

--- components/Page.js
+++ components/Page.js
@@ -1,19 +1,169 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import _ from 'lodash';
 
 import 'normalize.css/normalize.css';
 import '../css/main.css';
 
 import Ga from '../functions/Ga';
 
+import qData from '../question-data';
+
 export default class Page extends React.Component {
+
+	constructor() {
+		super();
+		this.state = {
+			subQuestion: false,
+			subQuestionNo: 0,
+			count: 0,
+			answersArray: [],
+			countLength: qData.length
+		}
+	}
+
+	getCurrentObj() {
+		if (this.state.subQuestion) {
+			return qData[this.state.count].options[this.state.subQuestionNo].subQuestion;
+		}
+
+		return qData[this.state.count];
+	}
+
+	renderQuestion() {
+		const currentObj = this.getCurrentObj();
+
+		return currentObj.question;
+	}
+
+	handelOptionClick(qKey, key) {
+		// console.log(this, qKey, key);
+		let newAnswersArray = this.state.answersArray;
+		newAnswersArray.push({qKey,key});
+		this.setState({
+			subQuestion: false,
+			count: this.state.count + 1,
+			answersArray: newAnswersArray
+		});
+	}
+
+	handelSubOptionClick(qKey, key) {
+		const currentObj = this.getCurrentObj();
+		let newAnswersArray = this.state.answersArray;
+		newAnswersArray.push({qKey,key});
+
+		let subQuestionIndex = _.findIndex(currentObj.options, (o) => {
+			return (
+				o.key == key
+			)
+		});
+
+
+		this.setState({
+			subQuestion: true,
+			subQuestionNo: subQuestionIndex,
+			answersArray: newAnswersArray
+		});
+	}
+
+	handelReset() {
+		this.setState({
+			subQuestion: false,
+			subQuestionNo: 0,
+			count: 0,
+			answersArray: []
+		});
+	}
+
+	renderOptions() {
+		const currentObj = this.getCurrentObj();
+		const qKey = currentObj.key;
+		const optionsArray = currentObj.options;
+
+		const options = optionsArray.map((val,i)=> {
+			const { image, text, key, subQuestion } = val;
+			if (subQuestion) {
+
+				return (
+					<div onClick={this.handelSubOptionClick.bind(this, qKey, key)} key={i}>
+						<img src={image} />
+						<div>{text}</div>
+					</div>
+				);
+			}
+			return (
+				<div onClick={this.handelOptionClick.bind(this, qKey, key)} key={i}>
+					<img src={image} />
+					<div>{text}</div>
+				</div>
+			);
+		});
+		return options;
+	}
+
+	renderCrumbs() {
+
+		const crumbsArray = this.state.answersArray.map((val)=> {
+			return val.key;
+		});
+
+		const crumbs = crumbsArray.map((val,i) => {
+			return (
+				<div key={i}>
+					{val}
+				</div>
+			);
+		});
+
+		return crumbs;
+	}
+
+	renderResult() {
+		return 'Result';
+	}
 
 	render() {
 
+		const styles = {
+			container: {
+				position: 'fixed',
+				top: 0,
+				left: 0,
+				width: '100%',
+				height: '100%',
+				backgroundColor: 'tomato',
+				overflow: 'auto'
+			}
+		}
+
 		return(
-			<div>
-			
-				Hello world!
+			<div style={styles.container}>
+
+				{(this.state.count >= this.state.countLength) 
+
+					?
+
+					<div>
+						<div>{this.renderCrumbs()}</div>
+
+						<div>{this.renderResult()}</div>
+						<div onClick={this.handelReset.bind(this)}>Reset finder</div>
+					</div>
+
+					:
+
+					<div>
+						<div>{this.renderCrumbs()}</div>
+						<div>
+							{this.renderQuestion()}
+						</div>
+
+						<div>
+							{this.renderOptions()}
+						</div>
+					</div>
+				}
+
 			</div>			
 		);
 
---
+++ question-data.js
@@ -0,0 +1,136 @@
+export default [
+
+	{
+		question: 'Question one Try it',
+		key: 'ANSWER Q1',
+		optional: false,
+		options: [
+			{
+				image: 'https://placehold.it/300x300&text=Option one',
+				text: 'This is option one',
+				key: 'q one',
+				subQuestion: 
+					{
+						question: 'Question 1a Try it',
+						key: 'ANSWER Q2',
+						optional: true,
+						options: [
+							{
+								image: 'https://placehold.it/300x300&text=Option one',
+								text: 'This is option one',
+								key: 'q one'
+							},
+							{
+								image: 'https://placehold.it/300x300&text=Option two',
+								text: 'This is option two',
+								key: 'q two'
+							},
+							{
+								image: 'https://placehold.it/300x300&text=Option three',
+								text: 'This is option three',
+								key: 'q three'
+							},
+							{
+								image: 'https://placehold.it/300x300&text=Option four',
+								text: 'This is option four',
+								key: 'q four'
+							},
+						]
+					},
+			},
+			{
+				image: 'https://placehold.it/300x300&text=Option two',
+				text: 'This is option two',
+				key: 'q two',
+				subQuestion: 
+					{
+						question: 'Question 1b Try it',
+						optional: true,
+						key: 'ANSWER Q2',
+						options: [
+							{
+								image: 'https://placehold.it/300x300&text=Option one',
+								text: 'This is option one',
+								key: 'q one'
+							},
+							{
+								image: 'https://placehold.it/300x300&text=Option two',
+								text: 'This is option two',
+								key: 'q two'
+							},
+							{
+								image: 'https://placehold.it/300x300&text=Option three',
+								text: 'This is option three',
+								key: 'q three'
+							},
+							{
+								image: 'https://placehold.it/300x300&text=Option four',
+								text: 'This is option four',
+								key: 'q four'
+							},
+						]
+					},
+			},
+			{
+				image: 'https://placehold.it/300x300&text=Option three',
+				text: 'This is option three',
+				key: 'q three'
+			}
+		]
+	},
+	{
+		question: 'Question two Try it',
+		optional: true,
+		key: 'ANSWER Q3',
+		options: [
+			{
+				image: 'https://placehold.it/300x300&text=Option one',
+				text: 'This is option one',
+				key: 'q one'
+			},
+			{
+				image: 'https://placehold.it/300x300&text=Option two',
+				text: 'This is option two',
+				key: 'q two'
+			},
+			{
+				image: 'https://placehold.it/300x300&text=Option three',
+				text: 'This is option three',
+				key: 'q three'
+			},
+			{
+				image: 'https://placehold.it/300x300&text=Option four',
+				text: 'This is option four',
+				key: 'q four'
+			},
+		]
+	},
+	{
+		question: 'Question four Try it',
+		optional: true,
+		key: 'ANSWER Q4',
+		options: [
+			{
+				image: 'https://placehold.it/300x300&text=Option one',
+				text: 'This is option one',
+				key: 'q one'
+			},
+			{
+				image: 'https://placehold.it/300x300&text=Option two',
+				text: 'This is option two',
+				key: 'q two'
+			},
+			{
+				image: 'https://placehold.it/300x300&text=Option three',
+				text: 'This is option three',
+				key: 'q three'
+			},
+			{
+				image: 'https://placehold.it/300x300&text=Option four',
+				text: 'This is option four',
+				key: 'q four'
+			},
+		]
+	},
+
+];
